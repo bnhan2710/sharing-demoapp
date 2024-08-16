14e86f9ec0d20c70e7cd150d33a329662b99e446
imlawcs <daolehanhnguyen@gmail.com>
2024-08-16 08:19:15 +0700

    feat: Add checkAuthToken function

--- src/utils/checkAuthToken.js
+++ src/utils/checkAuthToken.js
@@ -1,1 +1,24 @@
 //Check Token
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = process.env.SECRET_KEY;
+
+const checkAuthToken = async (token) => {
+    if(!token) return { statusCode: 401, message: 'Access denied' };
+    try {
+        const verifiedUser = await jwt.verify(token, jwtSecret);
+        if (!verifiedUser) {
+            return { statusCode: 403, message: 'Invalid token' };
+        }
+        const decoded = jwt.decode(token);
+        return { statusCode: 200, message: 'Token verified' , user: decoded };
+    }
+    catch(err) {
+        console.log(err);
+        return { statusCode: 500, message: 'Internal server error' };
+    }
+}
+
+module.exports = {
+    checkAuthToken
+}
